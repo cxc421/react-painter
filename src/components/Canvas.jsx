b97a9bd74999a43b29368ae253dca346a6f24663
Chris Chuang <chrischuang0421@gmail.com>
2019-02-27 14:23:38 +0800

    Add cursor

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -9,24 +9,112 @@ const Canvas = styled.canvas`
   width: 100%;
   height: 100%;
   background: #e8e8e8;
-  cursor: crosshair;
+  cursor: none;
 `;
 
-const resizeCanvas = (() => {
-  let preWidth = null;
-  let preHeight = null;
-  return canvas => {
+const renderLoop = (() => {
+  let canvas = null;
+  let ctx = null;
+  let dataImg = null;
+  let canUpdate = true;
+  let mode = 'mode_draw';
+  let mouseX = -100;
+  let mouseY = -100;
+  let color = '#000';
+  let size = 10;
+  let updateKey = null;
+
+  function resizeCanvas() {
     const { width, height } = canvas.getBoundingClientRect();
-    if (width !== preWidth || height !== preHeight) {
-      canvas.width = width;
-      canvas.height = height;
-      preWidth = width;
-      preHeight = height;
+    canvas.width = width;
+    canvas.height = height;
+  }
+
+  function drawCursor() {
+    if (mode === 'mode_draw') {
+      ctx.beginPath();
+      ctx.strokeStyle = color;
+      ctx.lineWidth = size;
+      ctx.lineJoin = 'round';
+      ctx.lineCap = 'round';
+      ctx.moveTo(mouseX, mouseY);
+      ctx.lineTo(mouseX, mouseY);
+      ctx.stroke();
+      return;
+    }
+
+    if (mode === 'mode_eraser') {
+      // ctx.beginPath();
+      // ctx.lineWidth = 6;
+      // ctx.arc(mouseX, mouseY, size / 2 - 2.5, 0, Math.PI * 2);
+      // ctx.strokeStyle = '#000';
+      // ctx.stroke();
+      // ctx.closePath();
+
+      ctx.beginPath();
+      ctx.arc(mouseX, mouseY, size / 2, 0, Math.PI * 2);
+      ctx.fillStyle = 'whitesmoke';
+      ctx.fill();
+      ctx.closePath();
+      return;
+    }
+  }
+
+  function update() {
+    if (!canUpdate) {
+      cancelAnimationFrame(updateKey);
+      updateKey = requestAnimationFrame(update);
+      return;
+    }
+    // console.log('u');
+    if (dataImg) {
+      let canvasPic = new Image();
+      canvasPic.src = dataImg;
+      canvasPic.onload = function() {
+        resizeCanvas();
+        ctx.drawImage(canvasPic, 0, 0);
+        drawCursor();
+        cancelAnimationFrame(updateKey);
+        updateKey = requestAnimationFrame(update);
+      };
+    } else {
+      resizeCanvas();
+      ctx.fillStyle = backgroundColor;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      drawCursor();
+      cancelAnimationFrame(updateKey);
+      updateKey = requestAnimationFrame(update);
+    }
+  }
+
+  return {
+    init(newCanvas) {
+      canvas = newCanvas;
+      ctx = canvas.getContext('2d');
+      update();
+    },
+    setCursor(x, y) {
+      mouseX = x;
+      mouseY = y;
+    },
+    setSpec(spec) {
+      color = spec.color;
+      size = spec.size;
+      mode = spec.mode;
+    },
+    setDataImg(newDataImg) {
+      dataImg = newDataImg;
+    },
+    stopUpdate() {
+      canUpdate = false;
+    },
+    startUpdate() {
+      canUpdate = true;
     }
   };
 })();
 
-function drawLineStart(canvas, x, y, color = '#000', size = 10) {
+function drawLineStart({ canvas, x, y, color = '#000', size = 10 }) {
   const ctx = canvas.getContext('2d');
   ctx.beginPath();
   ctx.strokeStyle = color;
@@ -38,26 +126,17 @@ function drawLineStart(canvas, x, y, color = '#000', size = 10) {
   ctx.stroke();
 }
 
-function drawLine(canvas, x, y) {
+function drawLine({ canvas, x, y, color, size }) {
   const ctx = canvas.getContext('2d');
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
   ctx.lineTo(x, y);
   ctx.stroke();
 }
 
-function drawDataImg(canvas, dataImg) {
-  resizeCanvas(canvas);
-  const ctx = canvas.getContext('2d');
-  var canvasPic = new Image();
-  canvasPic.src = dataImg;
-  canvasPic.onload = function() {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = backgroundColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(canvasPic, 0, 0);
-  };
-}
-
-function clearCanvas(canvas) {
+function clearCanvas({ canvas }) {
   const ctx = canvas.getContext('2d');
   // ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = backgroundColor;
@@ -88,71 +167,87 @@ export default () => {
   const canvasRef = useRef(null);
   const downloadTagRef = useRef(null);
   const [drawing, setDrawing] = useState(false);
-  const [size, setSize] = useState(4);
+  const [size, setSize] = useState(20);
   const [color, setColor] = useState('#000000');
   const [history, setHistory] = useState([]);
   const [historyIdx, setHistoryIdx] = useState(-1);
   const [mode, setMode] = useState('mode_draw');
 
-  // console.log({ history, historyIdx });
+  // compute update
   const canRedo = history.length > historyIdx + 1;
   const canUndo = historyIdx > -1;
 
-  // First Time Resize
+  // Init RenderLoop
   useEffect(() => {
-    resizeCanvas(canvasRef.current);
+    renderLoop.setSpec({ size, color, mode });
+    renderLoop.init(canvasRef.current);
   }, []);
 
-  // Resize
+  // Update RenderLoop Spec
   useEffect(() => {
-    const onResize = () => {
-      const dataImg = history[historyIdx];
-      if (dataImg) {
-        drawDataImg(canvasRef.current, dataImg);
-      } else {
-        resizeCanvas(canvasRef.current);
-      }
-    };
-    window.addEventListener('resize', onResize);
-    return () => {
-      window.removeEventListener('resize', onResize);
-    };
-  });
+    renderLoop.setSpec({ size, color, mode });
+  }, [size, color, mode]);
 
-  function pushHistory() {
+  function pushHistory(newDataImg) {
     let oldHistory = history.slice(0, historyIdx + 1);
-    setHistory([...oldHistory, canvasRef.current.toDataURL()]);
+    setHistory([...oldHistory, newDataImg]);
     setHistoryIdx(historyIdx + 1);
   }
 
   function onCanvasMouseDown(e) {
     setDrawing(true);
+    renderLoop.stopUpdate();
+
     const correctSize = checkSize(size);
-    const newColor = mode === 'mode_draw' ? color : backgroundColor;
     setSize(correctSize);
-    drawLineStart(
-      canvasRef.current,
-      e.clientX,
-      e.clientY,
-      newColor,
-      correctSize
-    );
+    const newColor = mode === 'mode_draw' ? color : backgroundColor;
+    drawLineStart({
+      canvas: canvasRef.current,
+      x: e.clientX,
+      y: e.clientY,
+      color: newColor,
+      size: correctSize
+    });
   }
 
   function onCanvasMouseMove(e) {
+    const x = e.clientX;
+    const y = e.clientY;
     if (drawing) {
-      drawLine(canvasRef.current, e.clientX, e.clientY);
+      const correctSize = checkSize(size);
+      if (size !== correctSize) {
+        setSize(correctSize);
+      }
+      const newColor = mode === 'mode_draw' ? color : backgroundColor;
+      drawLine({
+        canvas: canvasRef.current,
+        x,
+        y,
+        color: newColor,
+        size: correctSize
+      });
+    } else {
+      renderLoop.setCursor(x, y);
     }
   }
 
   function onCanvasMouseUp() {
+    let newDataImg = canvasRef.current.toDataURL();
+    pushHistory(newDataImg);
+    renderLoop.setDataImg(newDataImg);
+    renderLoop.startUpdate();
     setDrawing(false);
-    pushHistory();
+  }
+
+  function onCanvasMouseLeave() {
+    renderLoop.setCursor(-100, -100);
   }
 
   function onClickClearBtn() {
-    clearCanvas(canvasRef.current);
-    pushHistory();
+    clearCanvas({ canvas: canvasRef.current });
+    let newDataImg = canvasRef.current.toDataURL();
+    pushHistory(newDataImg);
+    renderLoop.setDataImg(newDataImg);
   }
 
   function onCheckSize() {
@@ -163,10 +258,12 @@ export default () => {
   function onClickUndoBtn() {
     if (canUndo) {
       if (historyIdx === 0) {
-        clearCanvas(canvasRef.current);
+        clearCanvas({ canvas: canvasRef.current });
+        let newDataImg = canvasRef.current.toDataURL();
+        renderLoop.setDataImg(newDataImg);
       } else {
         const dataImg = history[historyIdx - 1];
-        drawDataImg(canvasRef.current, dataImg);
+        renderLoop.setDataImg(dataImg);
       }
       setHistoryIdx(historyIdx - 1);
     }
@@ -175,7 +272,7 @@ export default () => {
   function onClickRedoBtn() {
     if (canRedo) {
       const dataImg = history[historyIdx + 1];
-      drawDataImg(canvasRef.current, dataImg);
+      renderLoop.setDataImg(dataImg);
       setHistoryIdx(historyIdx + 1);
     }
   }
@@ -195,6 +292,7 @@ export default () => {
         onMouseDown={onCanvasMouseDown}
         onMouseMove={onCanvasMouseMove}
         onMouseUp={onCanvasMouseUp}
+        onMouseLeave={onCanvasMouseLeave}
       />
       <TopPanel
         onClickClearBtn={onClickClearBtn}
